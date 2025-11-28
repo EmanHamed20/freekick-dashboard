2235dc799f4e69b2e43c6b917def0bb29d2aaf91
Fatma-Mahmoud-55 <113183957+Fatma-Mahmoud-55@users.noreply.github.com>
2025-11-28 13:35:22 +0200

    confirm popup

--- src/services/pitches/pitchesService.js
+++ src/services/pitches/pitchesService.js
@@ -1,18 +1,31 @@
 import api from '../api.js';
 import { toast } from 'react-toastify';
 
+
+// services/pitches/pitchesService.js
 export const pitchesService = {
     // view all pitches
     getAllPitchess: async () => {
         try {
             const response = await api.get('/venue/pitch/pitches/');
             toast.success("Pitches loaded successfully!");
-
             return response.data;
         } catch (error) {
             const message = error.response?.data?.message || "Failed to load pitches";
             toast.error(message);
+            throw error;
+        }
+    },
 
+    // delete pitch
+    deletePitch: async (id) => {
+        try {
+            const response = await api.delete(`/venue/pitch/pitches/${id}/`);
+            toast.success("Pitch deleted successfully!");
+            return response.data;
+        } catch (error) {
+            const message = error.response?.data?.message || "Failed to delete pitch";
+            toast.error(message);
             throw error;
         }
     },
